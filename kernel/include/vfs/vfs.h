5dc0dc2289e8748b833c67a8697ba5f9339cfd51
User Name <lemrdragons@gmail.com>
2014-02-10 16:42:29 +0000

    Implementing more vfs types, mount lists, etc

--- kernel/include/vfs/vfs.h
+++ kernel/include/vfs/vfs.h
@@ -3,13 +3,28 @@
 
 enum {
 	FILE_FLAG_NULL 		= 0,
-	FILE_FLAG_MOUNTED 	= 1,
+	FILE_FLAG_READ  	= 1,
+	FILE_FLAG_WRITE  	= 2,
+	FILE_FLAG_EXEC  	= 4,
+	FILE_FLAG_MOUNTED 	= 8,
+	FILE_FLAG_INCOMPLETE 	= 16,
+};
+
+enum {
+	FILE_LOOKUP_NULL 	= 0,
+	FILE_LOOKUP_LINKS 	= 1, // follow symlinks
+	FILE_LOOKUP_MOUNT 	= 2  // follow mount points
 };
 
 enum {
 	FILE_DRIVER_NULL,
 };
 
+typedef enum {
+	FILE_TYPE_NULL,
+} file_type_t;
+
+// Structure prototypes for the following function prototypes
 struct file_node;
 struct file_info;
 struct file_driver;
@@ -24,7 +39,7 @@ typedef int (*write_func)( struct file_node *, void *, unsigned long );
 typedef int (*read_func)( struct file_node *,  void *, unsigned long );
 typedef int (*pwrite_func)( struct file_node *, void *, unsigned long, unsigned long );
 typedef int (*pread_func)( struct file_node *, void *, unsigned long, unsigned long );
-typedef struct dirp *(*opendir_func)( struct file_node * );
+typedef int (*opendir_func)( struct file_node *, struct dirent *dirp );
 typedef int (*closedir_func)( struct file_node * );
 typedef int (*close_func)( struct file_node * );
 
@@ -34,12 +49,16 @@ typedef int (*link_func)( struct file_node *, struct file_node * );
 typedef int (*unlink_func)( struct file_node * );
 typedef int (*getdents_func)( struct file_node *, struct dirent *dirp, unsigned long count, unsigned long offset );
 typedef int (*readdir_func)( struct file_node *, struct dirent *dirp, int entry );
+typedef int (*mount_func)( struct file_node *, struct file_node *, int flags );
+typedef int (*lookup_func)( struct file_node *, struct file_node **, char *name, int flags );
 
 // Functions provided by drivers to manage filesystems
 typedef int (*file_create_fs)( struct file_driver *, struct file_system *, char *, unsigned flags );
 typedef int (*file_remove_fs)( struct file_driver *, struct file_system *, unsigned flags );
 
+// VFS structures
 typedef struct file_funcs {
+	// File system functions
 	read_func  	read;
 	pread_func  	pread;
 	write_func	write;
@@ -50,6 +69,7 @@ typedef struct file_funcs {
 	link_func	link;
 	unlink_func	unlink;
 	readdir_func	readdir;
+	lookup_func 	lookup;
 
 	open_func	open;
 	close_func	close;
@@ -58,14 +78,18 @@ typedef struct file_funcs {
 } file_funcs_t;
 
 typedef struct file_system {
-	void 	*devstruct; 	// Place for file driver to store it's precious memories
-
+	struct file_node *root_node; 	// Top node of file system
 	file_funcs_t *functions;
+
+	void 	*devstruct;  		// Place for file driver to store it's precious memories
 } file_system_t;
 
 typedef struct file_driver {
-	void 	*driver;
-	char 	*name;
+	unsigned 	references;
+	unsigned 	flags;
+
+	void 		*driver;
+	char 		*name;
 
 	file_create_fs 	*create;
 	file_remove_fs 	*remove;
@@ -73,15 +97,21 @@ typedef struct file_driver {
 
 typedef struct file_node {
 	unsigned 	inode;
+	unsigned 	flags;
+	unsigned 	refcount; 	// Number of references to this node
 
-	file_system_t 	*fs;
+	file_system_t 	*fs; 		// Owning file system
 } file_node_t;
+
+typedef struct file_mount {
+	unsigned 	flags; 		// Uses same flags as file_system_t
+
+	file_system_t  	*fs;
+} file_mount_t;
 
 typedef struct file_info {
 	file_type_t	type;
 
-	//char		name[ MAX_NAME_LEN ];
-	char		*name;
 	unsigned long 	mask;
 	unsigned long	uid;
 	unsigned long 	gid;
--- kernel/vfs/vfs.c
+++ kernel/vfs/vfs.c
@@ -1,12 +1,19 @@
+#include <vfs/vfs.h>
+#include <base/datastructs/list.h>
 #include <base/logger.h>
 
 char *depends[] = { "base", 0 };
 char *provides = "vfs";
 
+static list_node_t *driver_list = 0;
+static list_node_t *mount_list = 0;
+
 int init( ){
-	kprintf( "[%s] Hello world!\n", provides );
-	kprintf( "[%s] This is module \"%s\", and I'm in yo kernelz\n",
-			provides, provides );
+	kprintf( "[%s] Initializing virtual file system...", provides );
+
+	driver_list = list_add_data_node( driver_list, 0 );
+	mount_list = list_add_data_node( driver_list, 0 );
+
 	return 0;
 }
 
