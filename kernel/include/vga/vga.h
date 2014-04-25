8d3d90cc3806a4bebcab55316cb71c77b6f9bb4e
User Name <lemrdragons@gmail.com>
2014-04-25 05:24:52 -0500

    Added text-mode VGA driver

--- kernel/include/vga/vga.h
+++ kernel/include/vga/vga.h
@@ -1,5 +1,6 @@
 #ifndef _helix_vga_module_h
 #define _helix_vga_module_h
+#include <base/stdint.h>
 
 enum {
 	VGA_MODE_NULL,
@@ -7,9 +8,27 @@ enum {
 	VGA_MODE_FRAMEBUF,
 	VGA_MODE_VESA, 		// I know it's not part of vga, just
 				// easier to handle the code here
-}
+};
+
+enum {
+	VGA_COLOR_BLACK,
+	VGA_COLOR_BLUE,
+	VGA_COLOR_GREEN,
+	VGA_COLOR_CYAN,
+	VGA_COLOR_RED,
+	VGA_COLOR_MAROON,
+	VGA_COLOR_BROWN,
+	VGA_COLOR_GRAY,
+};
+
+enum {
+	VGA_COLOR_FOREGROUND = 0,
+	VGA_COLOR_BACKGROUND = 4,
+	VGA_COLOR_BOLD       = 8,
+};
 
 typedef struct vga_device {
+	void 		*buffer;
 	unsigned 	mode;
 	unsigned 	*baseaddr;
 	unsigned 	max_x;
@@ -19,5 +38,10 @@ typedef struct vga_device {
 	unsigned 	cur_x;
 	unsigned 	cur_y;
 } vga_device_t;
+
+typedef struct vga_char {
+	uint8_t letter;
+	uint8_t color;
+} __attribute__((packed)) vga_char_t;
 
 #endif
--- kernel/modules/vga/vga.c
+++ kernel/modules/vga/vga.c
@@ -1,12 +1,85 @@
+#include <vga/vga.h>
+#include <base/kstd.h>
+#include <base/hal.h>
 #include <base/logger.h>
+#include <base/string.h>
 
 char *depends[] = { "base", 0 };
 char *provides = "vga";
 
+static int vga_hal_write( struct hal_device *dev, void *buf, unsigned count, unsigned offset );
+
+/* Offset is ignored in this function, since it's handled by the driver */
+static int vga_hal_write( struct hal_device *dev, void *buf, unsigned count, unsigned offset ){
+	vga_device_t *vga = dev->dev;
+	vga_char_t *buffer = vga->buffer;
+	char *str = buf;
+	int ret = 0;
+	int i;
+
+	for ( ret = 0; ret < count; ret++ ){
+		switch ( str[ret] ){
+			case '\n':
+				vga->cur_y++;
+			case '\r':
+				vga->cur_x = 0;
+				continue;
+			default:
+				break;
+		}
+
+		if ( vga->cur_x == vga->max_x ){
+			vga->cur_x = 0;
+			vga->cur_y++;
+		}
+
+		if ( vga->cur_y >= vga->max_y ){
+			for ( i = 0; i < vga->max_y; i++ )
+				memcpy( buffer + i * vga->max_x, buffer + (i + 1) * vga->max_x,
+						sizeof( vga_char_t[vga->max_x] ));
+
+			vga->cur_y = vga->max_y - 1;
+		}
+
+		i = vga->cur_x + vga->cur_y * vga->max_x;
+
+		buffer[i].color = VGA_COLOR_GRAY;
+		buffer[i].letter = str[ret];
+
+		vga->cur_x++;
+	}
+
+	return ret;
+}
+
 int init( ){
-	kprintf( "[%s] Hello world!\n", provides );
-	kprintf( "[%s] This is module \"%s\", and I'm in yo kernelz\n",
-			provides, provides );
+	vga_device_t *new_vga;
+	hal_device_t *new_haldev;
+	vga_char_t *charbuf;
+	int i;
+
+	kprintf( "[%s] Hello, initializin' yo vga\n", provides );
+
+	new_vga = knew( vga_device_t );
+	charbuf = new_vga->buffer = (vga_char_t *)0xb8000;
+	new_vga->max_x = 80;
+	new_vga->max_y = 25;
+
+	new_haldev = knew( hal_device_t );
+	new_haldev->block_size = 1;
+	new_haldev->type = HAL_TYPE_VIDEO;
+	new_haldev->write = vga_hal_write;
+	new_haldev->dev = new_vga;
+	hal_register_device( new_haldev );
+
+	for ( i = 0; i < 80 * 25; i++ ){
+		charbuf[i].color = 0;
+		charbuf[i].letter = ' ';
+	}
+
+	char *teststr = "Cool, VGA text mode is working.\n";
+	vga_hal_write( new_haldev, teststr, strlen( teststr ), 0 );
+
 	return 0;
 }
 
