f48c6ecab94b75c47fbf8530a5c05f4588a0b6c1
My Name <nobody@mail.blox.xyz>
2015-05-07 12:41:42 +0000

    Fixing indentation

--- kernel/modules/vbe/vbe.c
+++ kernel/modules/vbe/vbe.c
@@ -19,49 +19,49 @@ extern initrd_t *main_initrd;
 static int vbe_hal_write( struct hal_device *dev, void *buf, unsigned count, unsigned offset );
 
 static void set_pixel( vbe_device_t *dev, unsigned x, unsigned y, uint32_t val ){
-    if ( x < dev->x_res && y < dev->y_res ){
-        uint32_t *place = (uint32_t *)(dev->framebuf + y * dev->pitch + x * 4);
-        *place = val;
-    }
+	if ( x < dev->x_res && y < dev->y_res ){
+		uint32_t *place = (uint32_t *)(dev->framebuf + y * dev->pitch + x * 4);
+		*place = val;
+	}
 }
 
 static void put_char( vbe_device_t *dev, unsigned c, unsigned xpos, unsigned ypos ){
-    psf2_header_t *font = dev->fontfile;
-    uint8_t *bitmap = (uint8_t *)font + dev->fontfile->header_size;
-    unsigned x, y;
-    unsigned mod = 1 << (font->width - 1);
-    uint32_t *place = (uint32_t *)(dev->framebuf + ypos * dev->pitch + xpos * 4);
+	psf2_header_t *font = dev->fontfile;
+	uint8_t *bitmap = (uint8_t *)font + dev->fontfile->header_size;
+	unsigned x, y;
+	unsigned mod = 1 << (font->width - 1);
+	uint32_t *place = (uint32_t *)(dev->framebuf + ypos * dev->pitch + xpos * 4);
 
-    for ( y = 0; y < font->height; y++ ){
-        for ( x = 0; x < font->width; x++ ){
-            if ( bitmap[c * font->charsize + y] & (mod >> x)){
-                *place = 0x00d0d0c0;
+	for ( y = 0; y < font->height; y++ ){
+		for ( x = 0; x < font->width; x++ ){
+			if ( bitmap[c * font->charsize + y] & (mod >> x)){
+				*place = 0x00d0d0c0;
 
-            } else {
-                *place = 0x00202020;
-            }
+			} else {
+				*place = 0x00202020;
+			}
 
-            place += 1;
-        }
-        
-        place += dev->x_res - font->width;
-    }
+			place += 1;
+		}
+
+		place += dev->x_res - font->width;
+	}
 }
 
 static void redraw_textbuf( vbe_device_t *dev, unsigned startx, unsigned starty ){
-    psf2_header_t *font = dev->fontfile;
-    uint8_t *buf = dev->textbuf;
-    unsigned x, y, c;
+	psf2_header_t *font = dev->fontfile;
+	uint8_t *buf = dev->textbuf;
+	unsigned x, y, c;
 
-    for ( y = starty; y < dev->text_y; y++ ){
-        for ( x = startx; x < dev->text_x; x++ ){
+	for ( y = starty; y < dev->text_y; y++ ){
+		for ( x = startx; x < dev->text_x; x++ ){
 			c = buf[y * dev->text_x + x];
 
-            if ( c ){
-                put_char( dev, c, x * font->width, y * font->height );
-            }
-        }
-    }
+			if ( c ){
+				put_char( dev, c, x * font->width, y * font->height );
+			}
+		}
+	}
 }
 
 static void clear_screen( vbe_device_t *dev ){
@@ -100,7 +100,7 @@ static int vbe_hal_write( struct hal_device *dev, void *buf, unsigned count, uns
 				vbe->cur_x--;
 
 				i = vbe->cur_x + vbe->cur_y * vbe->text_x;
-                textbuf[i] = '\b';
+				textbuf[i] = '\b';
 
 				//set_cursor( dev, vbe->cur_x, vbe->cur_y );
 				continue;
@@ -116,14 +116,14 @@ static int vbe_hal_write( struct hal_device *dev, void *buf, unsigned count, uns
 		if ( vbe->cur_y >= vbe->text_y - 1){
 			for ( i = 0; i < vbe->text_y - 1; i++ ){
 				// copy the next line of text in the framebuffer to the current line
-                memcpy(
-                    vbe->framebuf + ( i      * vbe->fontfile->height) * vbe->pitch,
-                    vbe->framebuf + ((i + 1) * vbe->fontfile->height) * vbe->pitch,
-                    vbe->pitch * vbe->fontfile->height );
+				memcpy(
+						vbe->framebuf + ( i      * vbe->fontfile->height) * vbe->pitch,
+						vbe->framebuf + ((i + 1) * vbe->fontfile->height) * vbe->pitch,
+						vbe->pitch * vbe->fontfile->height );
 
 				// update the text buffer accordingly
 				memcpy( textbuf + i * vbe->text_x, textbuf + (i + 1) * vbe->text_x, vbe->text_x );
-            }
+			}
 
 			vbe->cur_y = vbe->text_y - 2;
 		}
@@ -135,8 +135,8 @@ static int vbe_hal_write( struct hal_device *dev, void *buf, unsigned count, uns
 		//set_cursor( dev, vbe->cur_x, vbe->cur_y );
 	}
 
-    redraw_textbuf( vbe, startx, starty );
-    //redraw_textbuf( vbe, 0, 0 );
+	redraw_textbuf( vbe, startx, starty );
+	//redraw_textbuf( vbe, 0, 0 );
 
 	return ret;
 }
@@ -144,66 +144,66 @@ static int vbe_hal_write( struct hal_device *dev, void *buf, unsigned count, uns
 int init( ){
 	vbe_device_t *new_vbe;
 	hal_device_t *new_haldev;
-    vbe_mode_block_t *mode;
-    tar_header_t *tarhead;
+	vbe_mode_block_t *mode;
+	tar_header_t *tarhead;
 
 	kprintf( "[%s] Hello, initializin' yo vbe\n", provides );
 
 	new_vbe = knew( vbe_device_t );
 
-    new_vbe->mode = mode = (void *)bootheader->vbe_mode_info;
-    new_vbe->info = (void *)bootheader->vbe_control_info;
+	new_vbe->mode = mode = (void *)bootheader->vbe_mode_info;
+	new_vbe->info = (void *)bootheader->vbe_control_info;
 
-    tarhead = initrd_get_file( main_initrd, "kernel/config/vbefont.psf" );
+	tarhead = initrd_get_file( main_initrd, "kernel/config/vbefont.psf" );
 
-    if ( tarhead ){
-        new_vbe->fontfile = (psf2_header_t *)(tarhead + 1);
-    }
+	if ( tarhead ){
+		new_vbe->fontfile = (psf2_header_t *)(tarhead + 1);
+	}
 
-    kprintf( "[%s] info: 0x%x, mode: 0x%x, font: 0x%x\n",
-            provides, new_vbe->info, new_vbe->mode, new_vbe->fontfile );
+	kprintf( "[%s] info: 0x%x, mode: 0x%x, font: 0x%x\n",
+			provides, new_vbe->info, new_vbe->mode, new_vbe->fontfile );
 
-    if ( new_vbe->mode && new_vbe->info && tarhead ){
-        kprintf( "[%s] X res: %d, Y res: %d, bpp: %d, physaddr: 0x%x\n",
-                provides, mode->Xres, mode->Yres,
-                mode->bpp, mode->physbase );
+	if ( new_vbe->mode && new_vbe->info && tarhead ){
+		kprintf( "[%s] X res: %d, Y res: %d, bpp: %d, physaddr: 0x%x\n",
+				provides, mode->Xres, mode->Yres,
+				mode->bpp, mode->physbase );
 
-        kprintf( "[%s] fbconsole can hold %dx%d chars\n", __func__,
-                mode->Xres / new_vbe->fontfile->width,
-                mode->Yres / new_vbe->fontfile->height );
+		kprintf( "[%s] fbconsole can hold %dx%d chars\n", __func__,
+				mode->Xres / new_vbe->fontfile->width,
+				mode->Yres / new_vbe->fontfile->height );
 
-        new_vbe->text_x  = mode->Xres / new_vbe->fontfile->width;
-        new_vbe->text_y  = mode->Yres / new_vbe->fontfile->height;
-        new_vbe->textbuf = knew( uint8_t[new_vbe->text_y][new_vbe->text_x]);
+		new_vbe->text_x  = mode->Xres / new_vbe->fontfile->width;
+		new_vbe->text_y  = mode->Yres / new_vbe->fontfile->height;
+		new_vbe->textbuf = knew( uint8_t[new_vbe->text_y][new_vbe->text_x]);
 
-        new_vbe->framebuf = (void *)mode->physbase;
-        new_vbe->x_res    = mode->Xres;
-        new_vbe->y_res    = mode->Yres;
-        new_vbe->bpp      = mode->bpp;
-        new_vbe->pitch    = mode->pitch;
+		new_vbe->framebuf = (void *)mode->physbase;
+		new_vbe->x_res    = mode->Xres;
+		new_vbe->y_res    = mode->Yres;
+		new_vbe->bpp      = mode->bpp;
+		new_vbe->pitch    = mode->pitch;
 
-        unsigned pos = mode->physbase;
-        unsigned end = mode->physbase + mode->Xres * 4 + mode->Yres * mode->pitch;
+		unsigned pos = mode->physbase;
+		unsigned end = mode->physbase + mode->Xres * 4 + mode->Yres * mode->pitch;
 
-        for ( ; pos < end; pos += PAGE_SIZE ){
-            map_r_page( get_current_page_dir( ), pos, pos, PAGE_WRITEABLE | PAGE_PRESENT );
-        }
-        flush_tlb( );
+		for ( ; pos < end; pos += PAGE_SIZE ){
+			map_r_page( get_current_page_dir( ), pos, pos, PAGE_WRITEABLE | PAGE_PRESENT );
+		}
+		flush_tlb( );
 
 		clear_screen( new_vbe );
 
-        new_haldev = knew( hal_device_t );
-        new_haldev->block_size = 1;
-        new_haldev->type = HAL_TYPE_VIDEO;
-        new_haldev->write = vbe_hal_write;
-        new_haldev->dev = new_vbe;
-        //new_haldev->name = strdup( "framebuf" );
-        new_haldev->name = strdup( "fbconsole" );
-        hal_register_device( new_haldev );
+		new_haldev = knew( hal_device_t );
+		new_haldev->block_size = 1;
+		new_haldev->type = HAL_TYPE_VIDEO;
+		new_haldev->write = vbe_hal_write;
+		new_haldev->dev = new_vbe;
+		//new_haldev->name = strdup( "framebuf" );
+		new_haldev->name = strdup( "fbconsole" );
+		hal_register_device( new_haldev );
 
-    } else {
-        kprintf( "[%s] No mode/control/font info available, aborting...\n", provides );
-    }
+	} else {
+		kprintf( "[%s] No mode/control/font info available, aborting...\n", provides );
+	}
 
 	return 0;
 }
